53cb35f9fdd58e9bfafad2114a427ba65c4faa81
Fab-Moraes <progr.fabianomoraes@gmail.com>
2023-09-10 09:12:52 -0300

    estrRepeticao_for

---
+++ repeticao_for.py
@@ -0,0 +1,14 @@
+"""
+texto = input("Informe um texto: ")
+vogais = "AEIOU"
+
+for letra in texto:
+    if letra.upper () in vogais:
+        print(letra, end="")
+    
+print() # adiciona uma quebra de linha
+"""
+palavra = "Python"
+for letra in palavra:
+    if letra.upper() in palavra:
+    print(letra, end="")
